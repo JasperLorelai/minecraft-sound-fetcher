4b9882917958572131df6de7ea726677cc858591
JasperLorelai <wolf.haci12@gmail.com>
2025-04-24 23:16:18 +0200

    refactor: Read version from constant

--- src/main/java/eu/jasperlorelai/soundfetcher/SoundFetcher.java
+++ src/main/java/eu/jasperlorelai/soundfetcher/SoundFetcher.java
@@ -1,53 +1,35 @@
 package eu.jasperlorelai.soundfetcher;
 
 import java.util.List;
-import java.util.Scanner;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.lang.reflect.Field;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.BufferedWriter;
+import net.minecraft.SharedConstants;
 
 import io.papermc.paper.registry.TypedKey;
 import io.papermc.paper.registry.keys.SoundEventKeys;
 
 public class SoundFetcher {
 
-	@SuppressWarnings({"ResultOfMethodCallIgnored"})
 	public static void main(String[] args) {
+		@SuppressWarnings("deprecation")
+		Path soundFile = Paths.get(SharedConstants.VERSION_STRING + ".txt");
+
+		List<String> sounds = new ArrayList<>();
 		try {
-			Scanner scanner = new Scanner(System.in);
-			System.out.println("Enter file name: ");
-			String version = scanner.nextLine();
-			if (version == null || version.isEmpty()) return;
-
-			File soundFile = new File(version + (version.endsWith(".txt") ? "" : ".txt"));
-			soundFile.delete();
-			soundFile.createNewFile();
-
-			List<String> sounds = new ArrayList<>();
-			BufferedWriter writer = new BufferedWriter(new FileWriter(soundFile));
 			for (Field field : SoundEventKeys.class.getDeclaredFields()) {
-				try {
-					@SuppressWarnings("rawtypes")
-					TypedKey key = (TypedKey) field.get(null);
-					sounds.add(key.value());
-				} catch (Exception e) {
-					throw new RuntimeException(e);
-				}
+				@SuppressWarnings("rawtypes")
+				TypedKey key = (TypedKey) field.get(null);
+				sounds.add(key.value());
 			}
 
 			Collections.sort(sounds);
-			for (String sound : sounds) {
-				writer.append(sound);
-				writer.newLine();
-			}
-
-			writer.close();
-		} catch (IOException e) {
+			Files.write(soundFile, sounds);
+		} catch (Exception e) {
 			//noinspection CallToPrintStackTrace
 			e.printStackTrace();
 		}
