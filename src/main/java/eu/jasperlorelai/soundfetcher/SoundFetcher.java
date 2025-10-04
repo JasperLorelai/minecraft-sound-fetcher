10246c91c6ec4f9da99b86cb498a1d698a78c37b
JasperLorelai <wolf.haci12@gmail.com>
2025-10-04 18:15:46 +0200

    refactor: Read version from file

--- src/main/java/eu/jasperlorelai/soundfetcher/SoundFetcher.java
+++ src/main/java/eu/jasperlorelai/soundfetcher/SoundFetcher.java
@@ -1,14 +1,12 @@
 package eu.jasperlorelai.soundfetcher;
 
 import java.util.List;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Properties;
 import java.util.Collections;
 import java.lang.reflect.Field;
-
-import net.minecraft.SharedConstants;
 
 import io.papermc.paper.registry.TypedKey;
 import io.papermc.paper.registry.keys.SoundEventKeys;
@@ -16,11 +14,12 @@ import io.papermc.paper.registry.keys.SoundEventKeys;
 public class SoundFetcher {
 
 	public static void main(String[] args) {
-		@SuppressWarnings("deprecation")
-		Path soundFile = Paths.get(SharedConstants.VERSION_STRING + ".txt");
+		try (var stream = SoundFetcher.class.getClassLoader().getResourceAsStream("config.properties")) {
+			Properties properties = new Properties();
+			properties.load(stream);
+			String mcVersion = properties.getProperty("mcVersion");
 
-		List<String> sounds = new ArrayList<>();
-		try {
+			List<String> sounds = new ArrayList<>();
 			for (Field field : SoundEventKeys.class.getDeclaredFields()) {
 				@SuppressWarnings("rawtypes")
 				TypedKey key = (TypedKey) field.get(null);
@@ -28,7 +27,7 @@ public class SoundFetcher {
 			}
 
 			Collections.sort(sounds);
-			Files.write(soundFile, sounds);
+			Files.write(Paths.get(mcVersion + ".txt"), sounds);
 		} catch (Exception e) {
 			//noinspection CallToPrintStackTrace
 			e.printStackTrace();
